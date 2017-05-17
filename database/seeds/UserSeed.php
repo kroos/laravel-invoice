b559dfe23b9af0f7cc4a0a868013b316df6f4409
dhiauddin <dhiauddin@gmail.com>
2017-05-18 00:27:00 +0800

    - fixing preferences seed

--- database/seeds/UserSeed.php
+++ database/seeds/UserSeed.php
@@ -16,18 +16,21 @@ class UserSeed extends Seeder
 			'name' => 'Dhiauddin',
 			'email' => 'dhiauddin@gmail.com',
 			'password' => bcrypt('123123'),
+			'color' => 'rgba(250, 0, 0, 0.61)',
 			]);
 		DB::table('users')->insert([
 			'id_group' => 2,
 			'name' => 'Fatihah',
 			'email' => 'fatihah@gmail.com',
 			'password' => bcrypt('123123'),
+			'color' => 'rgba(102, 92, 214, 0.8)',
 			]);
 		DB::table('users')->insert([
 			'id_group' => 2,
 			'name' => 'Syazwani',
 			'email' => 'syazwani@gmail.com',
 			'password' => bcrypt('123123'),
+			'color' => 'rgba(150, 245, 106, 0.8)',
 			]);
     }
 }
