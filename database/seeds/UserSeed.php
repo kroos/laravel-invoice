c6b72cd1f91a24c3a9d9891808ef137250d32000
gopalvastarpara <vastarparagopal2412@gmail.com>
2019-07-25 15:57:26 +0530

    y

--- database/seeds/UserSeed.php
+++ database/seeds/UserSeed.php
@@ -1,43 +1,36 @@
 <?php
 
-// use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;
 
-// class UserSeed extends Seeder
-// {
-//     /**
-//      * Run the database seeds.
-//      *
-//      * @return void
-//      */
-//     public function run()
-//     {
-// 		DB::table('users')->insert([
-// 			'id_group' => 1,
-// 			'name' => 'Dhiauddin',
-// 			'slug' => 'Dhiauddin',
-// 			'username' => 'Dhiauddin',
-// 			'email' => 'dhiauddin@gmail.com',
-// 			'password' => bcrypt('123123'),
-// 			'color' => 'rgba(250, 0, 0, 0.61)',
-// 			]);
-// 		DB::table('users')->insert([
-// 			'id_group' => 2,
-// 			'name' => 'Fatihah',
-// 			'slug' => 'Fatihah',
-// 			'username' => 'Fatihah',
-// 			'email' => 'fatihah@gmail.com',
-// 			'password' => bcrypt('123123'),
-// 			'color' => 'rgba(102, 92, 214, 0.8)',
-// 			]);
-// 		DB::table('users')->insert([
-// 			'id_group' => 2,
-// 			'name' => 'Syazwani',
-// 			'slug' => 'Syazwani',
-// 			'username' => "Syazwani",
-// 			'email' => 'syazwani@gmail.com',
-// 			'password' => bcrypt('123123'),
-// 			'color' => 'rgba(150, 245, 106, 0.8)',
-// 			]);
-//     }
-// }
-?>
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		DB::table('users')->insert([
+			'id_group' => 1,
+			'name' => 'Dhiauddin',
+			'email' => 'dhiauddin@gmail.com',
+			'password' => bcrypt('123123'),
+			'color' => 'rgba(250, 0, 0, 0.61)',
+			]);
+		DB::table('users')->insert([
+			'id_group' => 2,
+			'name' => 'Fatihah',
+			'email' => 'fatihah@gmail.com',
+			'password' => bcrypt('123123'),
+			'color' => 'rgba(102, 92, 214, 0.8)',
+			]);
+		DB::table('users')->insert([
+			'id_group' => 2,
+			'name' => 'Syazwani',
+			'email' => 'syazwani@gmail.com',
+			'password' => bcrypt('123123'),
+			'color' => 'rgba(150, 245, 106, 0.8)',
+			]);
+    }
+}
