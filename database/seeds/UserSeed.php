765a20a3ca91d04084a14eddd49667a8fbfa6d5a
gopalvastarpara <vastarparagopal2412@gmail.com>
2019-07-25 16:59:24 +0530

    change this file "

--- database/seeds/UserSeed.php
+++ database/seeds/UserSeed.php
@@ -14,22 +14,28 @@ class UserSeed extends Seeder
 		DB::table('users')->insert([
 			'id_group' => 1,
 			'name' => 'Dhiauddin',
+			'username' => 'Dhiauddin',
 			'email' => 'dhiauddin@gmail.com',
 			'password' => bcrypt('123123'),
+			'slug' => 'null',
 			'color' => 'rgba(250, 0, 0, 0.61)',
 			]);
 		DB::table('users')->insert([
 			'id_group' => 2,
 			'name' => 'Fatihah',
+			'username' => 'Fatihah',
 			'email' => 'fatihah@gmail.com',
 			'password' => bcrypt('123123'),
+			'slug' => 'null',
 			'color' => 'rgba(102, 92, 214, 0.8)',
 			]);
 		DB::table('users')->insert([
 			'id_group' => 2,
 			'name' => 'Syazwani',
+			'username' => 'Syazwani',
 			'email' => 'syazwani@gmail.com',
 			'password' => bcrypt('123123'),
+			'slug' => 'null',
 			'color' => 'rgba(150, 245, 106, 0.8)',
 			]);
     }
