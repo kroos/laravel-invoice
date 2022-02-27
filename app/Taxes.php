0d9fc3a4c59a723e667755ecb094388a98b00b90
Dhiauddin <dhiauddin@gmail.com>
2022-02-28 01:07:38 +0800

    - remove sluggable

--- app/Taxes.php
+++ app/Taxes.php
@@ -7,28 +7,28 @@ namespace App;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 // load sluggable
-use Cviebrock\EloquentSluggable\Sluggable;
-use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+// use Cviebrock\EloquentSluggable\Sluggable;
+// use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Taxes extends Model
 {
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
 
-    use Sluggable;
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'taxes'
-            ]
-        ];
-    }
+    // use Sluggable;
+    // public function sluggable()
+    // {
+    //     return [
+    //         'slug' => [
+    //             'source' => 'taxes'
+    //         ]
+    //     ];
+    // }
 
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
+    // public function getRouteKeyName()
+    // {
+    //     return 'slug';
+    // }
 
 	public function taxessales() {
 		return $this->hasMany('App\Sales', 'id_sales');
